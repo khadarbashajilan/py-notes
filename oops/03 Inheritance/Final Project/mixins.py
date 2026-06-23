a8971d8b1a1c46dc2b499d215c7005162bdcdf81
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2026-06-23 14:26:29 +0530

    deleted oop files

--- oops/03 Inheritance/Final Project/mixins.py
+++
@@ -1,9 +0,0 @@
-
-class HealerMixin:
-    """Provides healing capabilities to any character class."""
-    def heal(self, target, amount):
-        if not self.is_alive:
-            print(f"❌ {self.name} cannot heal while defeated!")
-            return
-        target.health += amount
-        print(f"✨ {self.name} heals {target.name} for {amount} HP!")
